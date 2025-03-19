76dfad946218e696ce1c2de2a841435692fb9161
lunardunno <126363523+lunardunno@users.noreply.github.com>
2025-03-19 21:38:41 +0400

    Changing for Error 207
    Changing description and title for Error 207

--- client/core/controllers/serverController.cpp
+++ client/core/controllers/serverController.cpp
@@ -771,7 +771,7 @@ ErrorCode ServerController::isUserInSudo(const ServerCredentials &credentials, D
     ErrorCode error = runScript(credentials, replaceVars(scriptData, genVarsForScript(credentials)), cbReadStdOut, cbReadStdErr);
 
     if (credentials.userName != "root" && stdOut.contains("sudo:") && !stdOut.contains("uname:") && stdOut.contains("not found"))
-        return ErrorCode::SudoPackageIsNotPreinstalled;
+        return ErrorCode::ServerSudoPackageIsNotPreinstalled;
     if (credentials.userName != "root" && !stdOut.contains("sudo") && !stdOut.contains("wheel"))
         return ErrorCode::ServerUserNotInSudo;
     if (stdOut.contains("can't cd to") || stdOut.contains("Permission denied") || stdOut.contains("No such file or directory"))
