8d2a4990e77ecbcd2154bad6b0f42f5e83219175
lunardunno <126363523+lunardunno@users.noreply.github.com>
2025-03-19 13:45:32 +0400

    Update remove_container

--- client/server_scripts/remove_container.sh
+++ client/server_scripts/remove_container.sh
@@ -1,8 +1,9 @@
 sudo docker stop $CONTAINER_NAME;\
-sudo docker --version 2>/dev/null | grep -q podman && \
-  sudo systemctl disable --now container-$CONTAINER_NAME.service && \
-  sudo systemctl daemon-reload && sudo systemctl reset-failed && \
-  sudo rm -f /etc/systemd/system/container-$CONTAINER_NAME.service && \
+if [ -n "$(sudo docker --version 2>/dev/null | grep podman)" ]; then \
+  sudo systemctl disable --now container-$CONTAINER_NAME.service;\
+  sudo systemctl daemon-reload; sudo systemctl reset-failed;\
+  sudo rm -f /etc/systemd/system/container-$CONTAINER_NAME.service;\
   sudo systemctl daemon-reload;\
+fi;\
 sudo docker rm -fv $CONTAINER_NAME;\
 sudo docker rmi $CONTAINER_NAME
