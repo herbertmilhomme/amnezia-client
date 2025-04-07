3ecb3a9f737d369b29a10a960a00e8a931c325f4
lunardunno <126363523+lunardunno@users.noreply.github.com>
2025-04-07 05:25:52 +0400

    Extended description of error
    Added extended description for Error: ServerDockerNotSupported.

--- client/core/errorstrings.cpp
+++ client/core/errorstrings.cpp
@@ -25,7 +25,8 @@ QString errorString(ErrorCode code) {
     case(ErrorCode::ServerSudoPackageIsNotPreinstalled): errorMessage = QObject::tr("The sudo package is not pre-installed on the server"); break;
     case(ErrorCode::ServerUserDirectoryNotAccessible): errorMessage = QObject::tr("The server user's home directory is not accessible"); break;
     case(ErrorCode::ServerUserNotAllowedInSudoers): errorMessage = QObject::tr("Action not allowed in sudoers"); break;
-    case(ErrorCode::ServerUserPasswordRequired): errorMessage = QObject::tr("The user's password is required"); break;
+    case(ErrorCode::ServerUserPasswordRequired): errorMessage = QObject::tr("The user's password is required"); breakk;
+    case(ErrorCode::ServerDockerNotSupported): errorMessage = QObject::tr("Docker which is for the server is not supported"); break;
 
     // Libssh errors
     case(ErrorCode::SshRequestDeniedError): errorMessage = QObject::tr("SSH request was denied"); break;
